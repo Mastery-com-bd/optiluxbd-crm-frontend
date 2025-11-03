2a88223c8db7429665880e69271bc1d0df1d1161
RJannat <ramimk097@gmail.com>
2025-11-03 18:37:19 +0600

    added 2 lottie file in banner and the last section

--- app/(pages)/dashboard/admin/orders/page.tsx
+++ app/(pages)/dashboard/admin/orders/page.tsx
@@ -4,8 +4,11 @@ import { OrderTable } from "@/components/pages/dashboard/admin/orders/OrderTable
 const page = () => {
     return (
         <div className="m-4 lg:m-8">
-            <OrderStats />
-            <OrderTable />
+            <h3 className="text-xl mb-2 font-bold">Orders</h3>
+            <div>
+                <OrderStats />
+                <OrderTable />
+            </div>
         </div>
     );
 };
