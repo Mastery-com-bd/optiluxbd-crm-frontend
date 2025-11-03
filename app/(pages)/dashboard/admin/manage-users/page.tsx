e200238415a5a8b99c9cd5be735ab70c2d9fca88
theabsparrow <absbashar04@gmail.com>
2025-11-03 21:13:41 +0600

    user management done

---
+++ app/(pages)/dashboard/admin/manage-users/page.tsx
@@ -0,0 +1,11 @@
+import ManageUsers from "@/components/pages/dashboard/admin/manageUsers/ManageUsers";
+
+const ManageuserPage = () => {
+  return (
+    <div>
+      <ManageUsers />
+    </div>
+  );
+};
+
+export default ManageuserPage;
