e9f3826e3dc44bc622cd27ac9bfcc9f40ad6bcf4
RJannat <ramimk097@gmail.com>
2025-12-14 10:06:53 +0600

    added low stock page

---
+++ app/(pages)/dashboard/admin/products/low-stock-products/page.tsx
@@ -0,0 +1,11 @@
+import LowStockProducts from "@/components/pages/dashboard/products/lowStockProducts/LowStockProducts";
+
+const Page = () => {
+    return (
+        <div>
+            <LowStockProducts />
+        </div>
+    );
+};
+
+export default Page;
