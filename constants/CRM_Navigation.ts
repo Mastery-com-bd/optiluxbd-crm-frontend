4496ca8fe0c844edf8683f343217638c09c9b272
RJannat <ramimk097@gmail.com>
2025-12-15 12:16:09 +0600

    added all orders page and overview page

--- constants/CRM_Navigation.ts
+++ constants/CRM_Navigation.ts
@@ -176,6 +176,11 @@ export const crmRoutes: NavRoute[] = [
     ],
     children: [
       {
+        title: "Overview",
+        path: "/dashboard/admin/orders/overview",
+        permissions: ["ORDERS VIEW"],
+      },
+      {
         title: "All Orders",
         path: "/dashboard/admin/orders",
         permissions: ["ORDERS VIEW"],
