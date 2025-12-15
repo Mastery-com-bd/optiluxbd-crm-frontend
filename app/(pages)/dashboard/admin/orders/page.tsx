987c8ed43011b9ffe5671810f87dbbea5ff37bea
RJannat <ramimk097@gmail.com>
2025-12-15 10:15:05 +0600

    added piechart

--- app/(pages)/dashboard/admin/orders/page.tsx
+++ app/(pages)/dashboard/admin/orders/page.tsx
@@ -1,19 +1,28 @@
 import { MonthlyOrder } from "@/components/pages/dashboard/admin/orders/MonthlyOrder";
 import { OrderStats } from "@/components/pages/dashboard/admin/orders/OrderStats";
+import { OrderStatusRatio } from "@/components/pages/dashboard/admin/orders/OrderStatusRatio";
 import { OrderTable } from "@/components/pages/dashboard/admin/orders/OrderTable";
 
 const page = () => {
   return (
-    <div className="m-4 lg:m-8">
-      <h3 className="text-xl mb-2 font-bold">Orders</h3>
-      <div>
-        <OrderStats />
-        <div className="flex justify-between my-10">
-          <div className="w-1/2">
-            <MonthlyOrder />
+    <div className="m-4 lg:m-8 ">
+      <div className="max-w-[1140] mx-auto">
+        <h3 className="text-xl mb-2 font-bold">Orders</h3>
+        <div>
+          <OrderStats />
+          <div className="flex justify-between my-10 gap-4">
+            <div className="w-[70%]">
+              <MonthlyOrder />
+            </div>
+            <div className="w-[30%]">
+              <OrderStatusRatio />
+            </div>
           </div>
+          {/* <div>
+            <Demo />
+          </div> */}
+          <OrderTable />
         </div>
-        <OrderTable />
       </div>
     </div>
   );
